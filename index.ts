75005bde4449f63042ce36db988926db425122b8
Gulzaman <165961967+gulzaman98@users.noreply.github.com>
2025-03-14 22:17:54 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,49 @@
+import chalk from 'chalk';
+
+
+// IS CLASS  MAE HAM NAE PRHA k kis trha ham   ALAG ALAG NAM K VARIABLE AK HI LINE MAE BNA SKTY HAE US K LIYE HAMARY PAS 
+// AK METHOD HOTA NHAE JISY HAM ARRAY KHTY HAE IS K ANDR HAM ALAG ALAG NAM K VARIABLE BNA  KAR SKTY HAE . 
+
+
+// let studentName : String[] = ["Gul", "Zaman", "Qamar", "Zaman"]
+
+// YAE MINY AK ARRAY BNYA HAE studentName SAE OR IS K ANDR MAE NAE 4 NAM RKHY HAE  . 
+
+// AB ARRAY K ANDAR TYPESCRIPT K KUCH BUILT IN FUNCTION HOTY HAE  JISY (PUSH , POP , SHIF5T , UNSHIFT AND SPLICE )
+ 
+// PUSH    = PUSH JO  HAE WO LAST MAE AK VALUE KO STORE KAR DITA HAE JO B HAM USEY BOLYN GAE 
+
+// POP    = POPO JO HAE WO LAST WALI VALUE KO REMOVE KAR DAE GA JO B HOGI .
+
+// SHIFT   = SHIFT JO HAE WO FIRST WALI VALUE KO DEL KAR DAE GA JO B HOGI 
+
+// UNSHIFT   = UNSHIFT FIRST MA AK VALUE KO STORE KAR DAEGA JO B HAM BOLYN GAE 
+
+// SPLICE     = SPLICE JO HAE WO DARMYN SAE VALUE KO REMOVE B KAR DAE GA '
+//. ACHA SPLICE JO HAE WO SAB KAM KRTA HAE JO POP PUSH SHIFT UNSHIFT KRTY HAE YAE DARMYN SAE VALUE KO REMOVE B KAR 
+// SKTA HAE DARMYN NMAE VALUE KO ADD B KAR SKTA HAE LAsT MAE REMOVE KAR K OR ADD B KAR SKTA HAE 
+
+
+// studentName.push("Hamza")
+// studentName.pop()
+// studentName.shift()
+// studentName.unshift("Naveed")
+// studentName.splice(1,1,"Ahmed")
+// console.log(studentName)
+
+// console.log(studentName.length)
+
+
+
+
+console.log(chalk.blue('Hello world!'));
+console.log(chalk.green("index of 20 : "))
+
+
+
+
+
+
+
+
+
